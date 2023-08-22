f3cb45f2c9e6f718763545c060578603a1ad7ace
Guilherme <switchaep@gmail.com>
2023-08-21 23:17:10 -0300

    configurando ts

---
+++ app/models/negociacao.ts
@@ -0,0 +1,28 @@
+export class Negociacao {
+    // atributos privados de uma classe devem ser declarados assim:
+    private _data;
+    private _quantidade;
+    private _valor;
+
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+    get volume() {
+        return this._valor * this._quantidade;
+    }
+}
--- dist/js/app.ts
+++
@@ -1,4 +0,0 @@
-import { Negociacao } from "./models/negociacao.ts";
-
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.quantidade);
